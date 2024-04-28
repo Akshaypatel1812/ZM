0b4ff93dfba19adc7978d0f1b0ada0ca94997f44
Akshaypatel1812 <pak180301@gmail.com>
2024-04-28 20:49:08 +0530

    Merge Chnages

--- zenpaMarketing/src/components/Footer.jsx
+++ zenpaMarketing/src/components/Footer.jsx
@@ -105,6 +105,31 @@ export default function Footer() {
             {/* Other social media icons */}
           </div>
         </div>
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center">
+            © 2024
+            <a href="https://www.linkedin.com/in/ansh-mehta-171168249/" className="hover:underline">
+              developer contact
+            </a>
+            . Ansh Mehta
+          </span>
+          <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+            
+            <Link to="https://www.linkedin.com/in/ansh-mehta-171168249/" className="text-gray-500 hover:text-gray-900">
+              <svg
+                className="w-4 h-4"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20,0H4C1.8,0,0,1.8,0,4v16c0,2.2,1.8,4,4,4h16c2.2,0,4-1.8,4-4V4C24,1.8,22.2,0,20,0z M7.2,20H4.8V9.4h2.4V20z M6,8.2 c-0.7,0-1.2-0.5-1.2-1.2S5.3,5.8,6,5.8c0.7,0,1.2,0.5,1.2,1.2S6.7,8.2,6,8.2z M20,20h-2.4v-4.8c0-1.1,0-2.5-1.6-2.5 c-1.6,0-1.8,1.2-1.8,2.4V20h-2.4V9.4h2.2v1.3h0.1c0.3-0.6,1-1.2,2.1-1.2c2.3,0,2.7,1.5,2.7,3.4V20z"></path>
+              </svg>
+              <span className="sr-only">LinkedIn page</span>
+            </Link>
+
+            {/* Other social media icons */}
+          </div>
+        </div>
       </div>
     </footer>
   );
