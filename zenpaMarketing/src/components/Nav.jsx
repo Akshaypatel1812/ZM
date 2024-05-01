ced6149041f81eba0f8efaee2c69d6b0f2b0c1a1
Akshaypatel1812 <pak180301@gmail.com>
2024-05-01 13:47:25 +0530

    Install Some Packages

--- zenpaMarketing/src/components/Nav.jsx
+++ zenpaMarketing/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import {Menu, X} from 'lucide-react'
+import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import {
@@ -10,67 +10,78 @@ import {
 
 const activeClassName = "selected navlink";
 
-
-
 const NavLinks = () => {
-    return (
-        <>
-             <NavLink to='/' 
-            className={({isActive}) =>
-                                         
-            `${isActive? "text-orange-700" : "text-gray-700"} ` }
-            id='nav'
-        >Home</NavLink>
-        <NavLink to='/aboutUs' className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}`}
-                                         id='nav'
-                                         >About Us</NavLink>
-        <NavLink to='/ContactUs' className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}`}
-                                         id='nav'
-                                         >Contact Us</NavLink>
-        <NavLink to='/location' className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}`}
-                                         id='nav'
-                                         >Visit Us</NavLink>
-        <NavLink to='/Products' className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"} text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm  px-5 mt-[-0.35rem] py-2  mr-2 focus:outline-none`}
-                                         
-                                         >Our Products</NavLink>
-        </>
-    )
-}
+  return (
+    <>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `${isActive ? "text-orange-700" : "text-gray-700"} `
+        }
+        id="nav"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/aboutUs"
+        className={({ isActive }) =>
+          `${isActive ? "text-orange-700" : "text-gray-700"}`
+        }
+        id="nav"
+      >
+        About Us
+      </NavLink>
+      <NavLink
+        to="/ContactUs"
+        className={({ isActive }) =>
+          `${isActive ? "text-orange-700" : "text-gray-700"}`
+        }
+        id="nav"
+      >
+        Contact Us
+      </NavLink>
+      <NavLink
+        to="/location"
+        className={({ isActive }) =>
+          `${isActive ? "text-orange-700" : "text-gray-700"}`
+        }
+        id="nav"
+      >
+        Visit Us
+      </NavLink>
+      <NavLink
+        to="/Products"
+        className={({ isActive }) =>
+          `${
+            isActive ? "text-orange-700" : "text-gray-700"
+          } text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm  px-5 mt-[-0.35rem] py-2  mr-2 focus:outline-none`
+        }
+      >
+        Our Products
+      </NavLink>
+    </>
+  );
+};
 
-const Nav = () =>{
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen , setIsOpen] = useState(false)
-    
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleNavbar= ()=>{
-        setIsOpen(!isOpen);
-    }
+  return (
+    <>
+      <nav className="w-5/12 flex justify-end mr-4">
+        <div className="  hidden  w-full  justify-between md:flex  ">
+          <NavLinks />
+        </div>
+        <div className="md:hidden ">
+          <button onClick={toggleNavbar}>{isOpen ? <X /> : <Menu />}</button>
+        </div>
+      </nav>
 
-    return (
-        <>     
-    <nav  className="w-5/12 flex justify-end mr-4">
-       <div className="  hidden  w-full  justify-between md:flex  ">
-            <NavLinks/>
-       </div>
-       <div className="md:hidden ">
-        <button onClick={toggleNavbar}>
-            {isOpen ? <X /> : <Menu />}
-        </button>
-       </div>
-    </nav>
-
- 
-
-
-<AnimatePresence mode="wait">
+      <AnimatePresence mode="wait">
         {isOpen && (
           <motion.div
             layout="position"
@@ -81,47 +92,62 @@ const Nav = () =>{
             className="mt-4 basis-full  flex-col flex items-center  md:hidden"
           >
             <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
-              <NavLink to="/" className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"} `} >
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `${isActive ? "text-orange-700" : "text-gray-700"} `
+                }
+              >
                 Home
               </NavLink>
             </motion.div>
             <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
-              <NavLink to="/aboutUs" className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}  `}>
+              <NavLink
+                to="/aboutUs"
+                className={({ isActive }) =>
+                  `${isActive ? "text-orange-700" : "text-gray-700"}  `
+                }
+              >
                 About
               </NavLink>
             </motion.div>
             <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
-              <NavLink to="/ContactUs" className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"} `}>
+              <NavLink
+                to="/ContactUs"
+                className={({ isActive }) =>
+                  `${isActive ? "text-orange-700" : "text-gray-700"} `
+                }
+              >
                 Contact
               </NavLink>
             </motion.div>
             <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
-              <NavLink to="/location" className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}`}>
+              <NavLink
+                to="/location"
+                className={({ isActive }) =>
+                  `${isActive ? "text-orange-700" : "text-gray-700"}`
+                }
+              >
                 Visit Us
               </NavLink>
             </motion.div>
             <motion.div variants={mobileNavListVariant} {...mobileNavExitProps}>
-              <NavLink to="/Products" className={({isActive}) =>
-                                         
-                                         `${isActive? "text-orange-700" : "text-gray-700"}
+              <NavLink
+                to="/Products"
+                className={({ isActive }) =>
+                  `${isActive ? "text-orange-700" : "text-gray-700"}
                                          text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm  px-5 mt-[-0.35rem] py-2  mr-2 focus:outline-none
-                                         `}>
+                                         `
+                }
+              >
                 Our Products
               </NavLink>
             </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default  Nav
+export default Nav;
