50393bf8ab0e94b359cbc19af5784f5d9ab2aa48
Akshaypatel1812 <pak180301@gmail.com>
2024-07-26 01:52:32 +0530

    Finally Add The Catalogue Button

--- zenpaMarketing/src/components/Home.jsx
+++ zenpaMarketing/src/components/Home.jsx
@@ -641,30 +641,27 @@ export default function Home() {
                   </p>
 
                   <a
-                    href="\ZM Product Catalogue.pdf" // Path to the PDF file in the public directory
+                    href="/ZM Product Catalogue.pdf" // Correct path to the PDF file in the public directory
                     download="ZM Product Catalogue.pdf" // Name of the file when downloaded
-                    className="btn btn-primary" // Tailwind CSS class for styling
+                     // Tailwind CSS class for styling
                   >
-                    Download PDF
-                  </a>
-                  <div className="mt-7 flex justify-center lg:justify-start">
-                    <button className="relative flex items-center justify-start bg-lightBlue text-white py-[14px] px-[18px] rounded-md font-mullish hover:bg-lightBlue500 transition-all duration-200 w-[9rem] h-12">
-                      Explore Products
+                    <button className="relative flex items-center justify-start bg-blue-500 text-white py-[14px] px-[15px] rounded-md font-mullish hover:bg-blue-500 transition-all duration-200 w-[16rem] h-12 mt-8 font-bold">
+                      Zenpa Product Catalogue
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-arrow-right ml-2"
-                        viewBox="0 0 16 16"
+                        className="w-5 h-6 ml-3"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                        />
+                        <path d="M12 3v12.5m-6.5-6.5l6.5 6.5 6.5-6.5M21 20H3"></path>
                       </svg>
                     </button>
-                  </div>
+                  </a>
+                  
                 </div>
               </div>
             </div>
