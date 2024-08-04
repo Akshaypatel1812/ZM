1a7c71f4521fbc17e96e3df23982c1c359e34d85
Akshaypatel1812 <pak180301@gmail.com>
2024-08-05 02:13:29 +0530

    Final TatvaSoft

--- zenpaMarketing/src/main.jsx
+++ zenpaMarketing/src/main.jsx
@@ -14,7 +14,10 @@ import SafetyHelmet from './components/SafetyHelmet.jsx'
 import EyeProtection from './components/EyeProtection.jsx';
 import RespiratoryProtection from './components/RespiratoryProtection.jsx';
 import HandProtection from './components/HandProtection.jsx';
-
+import SmokeDetectorSystem from './components/SmokeDetectorSystem.jsx';
+import CCTVCameras from './components/CCTVCameras.jsx';
+import BreathingApparatus from './components/BreathingApparatus.jsx';
+import FireFightingProjectWork from './components/Fire FightingProjectWork.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
@@ -25,9 +28,14 @@ const router = createBrowserRouter(
       <Route path="/location" element={<Location />} />
       <Route path="/product/2" element={<FireExtinguisher />} />
       <Route path="/product/1" element={< SafetyHelmet/>} />
+      <Route path="/product/3" element={< FireFightingProjectWork/>} />
       <Route path="/product/4" element={< EyeProtection/>} />
       <Route path="/product/5" element={< RespiratoryProtection/>} />
       <Route path="/product/6" element={< HandProtection/>} />
+      <Route path="/product/7" element={< SmokeDetectorSystem/>} />
+      <Route path="/product/8" element={< CCTVCameras/>} />
+      <Route path="/product/9" element={< BreathingApparatus/>} />
+
     </Route>
   )
 );
